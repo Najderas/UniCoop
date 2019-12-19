6abb3ac403d2b535665c04f663bd4e56631dcc57
Lukasz <lbialas2@gmail.com>
2019-12-19 13:31:30 +0100

    updated player prefab with new controls

---
+++ Assets/Scripts/player.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+	public int speed = 5;
+	public int jump = 300;
+	public bool grounded;
+
+	public Rigidbody rb;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		Move();
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		grounded = true;
+	}
+
+
+
+	// Move the player
+	void Move()
+	{
+		if (grounded)
+		{
+			transform.Translate(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0, 0);
+			transform.Translate(0,0,Input.GetAxis("Vertical") * speed * Time.deltaTime);
+		}
+		if (Input.GetButtonDown("Jump") & grounded)
+		{
+			rb.AddForce(Input.GetAxis("Horizontal") * speed * Time.deltaTime, jump, 0);
+			grounded = false;
+		}
+
+	}
+
+}
--- Assets/Scripts/player_control_prototype1.cs
+++ Assets/Scripts/player_control_prototype1.cs
@@ -4,29 +4,29 @@ using UnityEngine;
 
 public class player_control : MonoBehaviour
 {
-    public bool grounded;
-	public Rigidbody rb;
-	public int speed;
-	public int jump;
-	
+    private bool grounded;
+	private Rigidbody rb;
+	public int speed = 600;
+	public int jump = 150;
+
 	// Start is called before the first frame update
-    void Start()
+	void Start()
     {
         rb = GetComponent<Rigidbody>();
 		grounded = true;
-		speed = 6;
-		jump = 50;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+		Move();
     }
 	
 	void Move()
 	{
 		rb.AddTorque( 0, Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0);
+		float forward_axis = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+		rb.AddForce(transform.forward * forward_axis);
 
 		if (Input.GetButtonDown("Jump") & grounded)
 		{
@@ -34,5 +34,10 @@ public class player_control : MonoBehaviour
 			grounded = false;
 		}
 		
+	}
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		grounded = true;
 	}
 }
