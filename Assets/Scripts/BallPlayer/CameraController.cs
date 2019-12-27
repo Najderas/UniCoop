d419d6d28ed5b705f63d6192900c0bbc661aaf02
Grzegorz Najderek <najderass@gmail.com>
2019-12-27 19:11:27 +0100

    camera controller, player init script, NetworkManager

---
+++ Assets/Scripts/BallPlayer/CameraController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour, ICameraController
+{
+
+    public GameObject player;
+
+    public Vector3 offset = new Vector3(0, 8, -10);
+    public float speed = 5f;
+
+    public void Init(GameObject player)
+    {
+        this.player = player;
+    }
+
+    // Use this for initialization
+    void Start ()
+	{
+        //offset = transform.position - player.transform.position;
+        //offset = new Vector3(0, 8, -10);
+	}
+	
+	// Update is called once per frame
+	void LateUpdate ()
+	{
+        if (!player) return;
+
+        //transform.position = player.transform.position + offset;
+
+        transform.position = Vector3.Lerp(transform.position, player.transform.position + offset, Time.deltaTime * speed);
+    }
+
+}
---
+++ Assets/Scripts/SetupPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class SetupPlayer : NetworkBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (isLocalPlayer)
+        {
+            var controller = GetComponent<PlayerController>();
+            if (controller) controller.enabled = true;
+
+            Camera.main.gameObject.GetComponent<ICameraController>().Init(gameObject);
+        }
+        else
+        {
+            var controller = GetComponent<PlayerController>();
+            if (controller) controller.enabled = false;
+        }
+    }
+
+}
